486a57a9359a522017481dbc665ccb44530b71b6
Chart <Chartfizz@outlook.com>
2022-12-27 16:44:54 +0700

    ็happy new year

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -2,10 +2,22 @@
 #include<iomanip> //For using setw(), setprecision(), ...
 using namespace std;
 
-int main(){	
-	cout << "Enter initial loan: ";
-	cout << "Enter interest rate per year (%): ";
-	cout << "Enter amount you can pay per year: ";
+int main(){
+
+    double loan;
+    double rate;
+    double pay;
+    double total,inter, newba;
+    int year=0;
+    
+     
+	cout << "Enter initial loan: " ;
+    cin >> loan;
+	cout << "Enter interest rate per year (%): " ;
+    cin >> rate;
+	cout << "Enter amount you can pay per year: " ;
+    cin >> pay;
+
 
 	//use 'setw' to set width of table and 'left' to set left-alignment
 	//you can change input argument of 'setw()' to see the effect
@@ -20,14 +32,38 @@ int main(){
 	
 	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
 	//you can change input argument of 'setprecision()' to see the effect
-	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
 	
-	return 0;
+    do{
+        year+=1;
+        loan=loan;
+        inter=loan*(rate/100);
+        total = loan+inter;
+        if(total>pay)
+        {
+            newba=total-pay;
+        }else if (total<=pay){
+            pay=total;
+            newba=total-total;
+        }
+    
+    
+        cout << fixed << setprecision(2); 
+	    cout << setw(13) << left << year; 
+	    cout << setw(13) << left << loan;
+	    cout << setw(13) << left << inter;
+	    cout << setw(13) << left << total;
+	    cout << setw(13) << left << pay;
+	    cout << setw(13) << left << newba;
+	    cout << "\n";	
+
+        if(newba>0){
+            loan=newba;
+            
+        }
+        else
+        {
+            break;
+        }
+    } while(1);
+return 0;
 }
--- lab10_2.cpp
+++ lab10_2.cpp
@@ -1,24 +1,20 @@
 #include<iostream>
+#include<string>
 using namespace std;
-
-//Write the function printO() here
-
+void printO (int N,int M){
+    if(N > 0 && M > 0){
+        for(int j =0 ; j<N ; j++){
+            for(int i=0 ; i<M ; i++){
+                cout << 'O';
+            }
+            
+           cout << '\n'; 
+           
+        }
+        
+    }else{cout << "Invalid input";}
+}
 int main(){
-	
-	printO(2,2);
-	cout << "\n";
-	
-	printO(3,5);
-	cout << "\n";	
-	
-	printO(5,3);
-	cout << "\n";	
-	
-	printO(0,3);
-	cout << "\n";	
-	
-	printO(7,-1);
-	cout << "\n";	
-	
-	return 0;
+    printO(5,-4);
+
 }
